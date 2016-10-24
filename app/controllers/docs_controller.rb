5c25a8de0d97c0a1e06d5a5a0ad6bfd1b58130ab
Chris Mendez <mail@chrisaiv.com>
2016-10-24 06:35:44 -0700

    added Docs controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,35 @@
+class DocsController < ApplicationController
+  
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'docs/index'
+
+  get 'docs/show'
+
+  get 'docs/new'
+
+  get 'docs/create'
+
+  get 'docs/edit'
+
+  get 'docs/update'
+
+  get 'docs/destroy'
+
+  get 'docs/find_doc'
+
+  get 'docs/doc_params'
+
   root to: 'visitors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :docs
 end
