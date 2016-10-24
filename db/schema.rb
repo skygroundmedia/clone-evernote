5d11ea95509e813975484e3f0a1612f613a653ac
Chris Mendez <mail@chrisaiv.com>
2016-10-24 11:28:16 -0700

    fixed mistake and user_id has now been added to the docs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024181713) do
+ActiveRecord::Schema.define(version: 20161024182400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161024181713) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
