585aa846b786dd6a25df0028a1267129a9a04aed
Chris Mendez <mail@chrisaiv.com>
2016-10-24 06:54:29 -0700

    Added /app/views/docs/_form and configured docs_controller.rb

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,4 @@
-class DocsController < ApplicationController
-  
+class DocsController < ApplicationController  
   def index
   end
 
@@ -10,9 +9,16 @@ class DocsController < ApplicationController
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -30,5 +36,7 @@ class DocsController < ApplicationController
   end
 
   def doc_params
+    params.require(:doc).permit(:title, :content)
   end
 end
+
